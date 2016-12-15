730561e1b33c810b4023a15a4761fca5615d75db
Flurin Egger <flurin@digitpaint.nl>
2016-12-15 10:48:28 +0100

    Plugin setup

---
+++ dp-user-docs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User documentation as markdown rendered right into WordPress
+ *
+ * @package   dp_user_docs
+ * @author    Flurin Egger <flurin@digitpaint.nl>
+ * @license   MIT
+ * @link      .
+ * @copyright 2016 Digitpaint
+ *
+ * @wordpress-plugin
+ * Plugin Name:       DP User Docs
+ * Plugin URI:        https://github.com/digitpaint/wordpress-dp-user-docs
+ * Description:       User documentation rendered as markdown right in your admin
+ * Version:           1.0.0
+ * Author:            Flurin Egger, Digitpaint
+ * Author URI:        https://github.com/digitpaint/wordpress-dp-user-docss
+ * Text Domain:       dp-user-docs
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ */
